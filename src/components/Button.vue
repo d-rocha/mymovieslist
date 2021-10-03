7d3c1b53b73d07386eacfd9fbed82c97295169b4
d-rocha <dr.oliveira88@gmail.com>
2021-10-03 12:32:06 -0300

    :sparkles: Applying style on Button component

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -4,6 +4,33 @@
     </button>
 </template>
 
+<style>
+    button {
+        border: none;
+        background: #2c3e50;
+        border-radius: 0.25em;
+        padding: 12px 20px;
+        color: #ffffff;
+        font-weight: bold;
+        float: right;
+        cursor: pointer;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        appearance: none;
+        cursor: pointer;
+        text-transform: uppercase;
+    }
+    button:hover {
+        background-color: #e12236;
+    }
+    button:focus {
+        outline: none;     
+    }
+    button:disabled {
+        opacity: 0.5;
+    }
+</style>
+
 <script>
     export default {
         name: 'Button',

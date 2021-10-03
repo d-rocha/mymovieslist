0817fb94729dbb6b438184bd4ecf10ce5f8c907c
d-rocha <dr.oliveira88@gmail.com>
2021-10-03 12:33:13 -0300

    :sparkles: Update Home component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,24 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <Movies titleMovie="Lista de Filmes"/>
+    <Movies />
   </div>
 </template>
 
-<script>
-import Movies from '@/components/Movies.vue'
-
-export default {
-  name: 'Home',
-  components: {
-    Movies
+<style>
+  .home {
+    width: 100vw;
+    height: 80vh;
+    margin-top: 20px;
   }
+</style>
+
+<script>
+  import Movies from '@/components/Movies.vue';
+
+  export default {
+    name: 'Home',
+    components: {
+      Movies
+    }
 }
 </script>

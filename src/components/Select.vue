ab03eb65d6064bb59e8e9cc22b96edcaa8963b37
d-rocha <dr.oliveira88@gmail.com>
2021-10-03 12:36:13 -0300

    :sparkles: Create Select component

---
+++ src/components/Select.vue
@@ -0,0 +1,64 @@
+<template>    
+    <p>{{title}}</p>
+    <select name="filter" class="filter" id="filter"  @change="handleChange($event)"  v-model="val">
+        <option value="">Escolha uma opção</option>
+        <option 
+            v-for="item in genderOption" 
+            :key="item" 
+            :value="item"
+        >
+            {{item}}
+        </option>
+    </select>
+    
+</template>
+
+<style>
+    select {
+        position: relative;
+        display: block;
+        width: 100%;
+        cursor: pointer;        
+        color: #2b3e51;
+        padding: 12px;
+        border: 1px solid #cfd9db;
+        background-color: #ffffff;
+        border-radius: 0.25em;        
+        box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.08);
+    }
+    select:focus {
+        outline: none;
+        border-color: #2c3e50;
+        box-shadow: 0 0 5px rgba(44, 151, 222, 0.2);
+    }
+</style>
+
+<script>
+    export default {
+        name: 'Select',
+        props: {
+            title: String,
+        },
+        data() {
+            return {
+                genderOption: [                    
+                    'Ação',
+                    'Animação',
+                    'Aventura',
+                    'Comédia',
+                    'Documentário',
+                    'Ficção',
+                    'Heróis',
+                    'Romance'
+                ],
+                selected: false,
+                val: ''
+            }
+        },
+        methods: {
+            handleChange(e) {
+                console.log('mexeu', e.target.value);
+            }
+        }
+    }
+</script>

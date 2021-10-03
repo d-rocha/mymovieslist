a38645ed15ec0766b7ef7486b8b5012c4700d96a
d-rocha <dr.oliveira88@gmail.com>
2021-10-03 12:30:19 -0300

    :sparkles: Create a Header's and App's component

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,34 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link>    
-  </div>
+  <Header />
   <router-view/>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+  body {  
+    margin: 0;
+    padding: 0;
+    color: #fff;  
+    background: linear-gradient(to right, #ED213A, #93291E);
+    width: 100%;
+    height: 100%;
+  }
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    height: 100%;
+  }
 </style>
+
+<script>
+  import Header from '@/components/Header.vue'
+
+  export default {
+    name: 'App',
+    components: {
+      Header
+    }       
+  }
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,69 @@
+<template>
+    <div id="nav">
+        <div class="logo">
+            <router-link to="/">
+                <img 
+                    src='../assets/logo.png'
+                    alt="Minha lista de filmes logo" 
+                    title="Minha lista de filmes logo" 
+                />
+            </router-link>
+        </div>
+        <ul class="header-links">
+            <li>
+                <router-link to="/">Home</router-link>
+            </li>
+            <li>
+                <router-link to="/newmovie">Cadastrar Filme</router-link>
+            </li>
+        </ul>
+  </div>
+</template>
+
+<style>
+    #nav {
+        display: flex;
+        justify-content: flex-start;
+        width: 96.8%;
+        padding: 10px 30px;
+        background-color: #e0e0e0;
+    }
+    #nav .header-links {
+        display: flex;
+        text-decoration: none;
+    }
+    #nav .header-links li {
+        margin-right: 30px;
+        list-style: none;
+    }
+    #nav a {
+        font-weight: bold;
+        text-decoration: none;
+        color: #2c3e50;
+        text-transform: uppercase;
+    }
+    #nav a.router-link-exact-active {        
+        color: #e92239;
+    }
+    #nav .logo {
+        margin-left: 400px;
+    }
+    #nav .logo img {
+        width: 50px;
+    }
+    @media screen and (max-width: 1440px) {
+        #nav .logo {
+            margin-left: 270px;
+        }
+    }
+    @media screen and (max-width: 1368px) {
+        #nav .logo {
+            margin-left: 260px;
+        }
+    }    
+</style>
+<script>
+    export default {
+        name: 'Header'        
+    }
+</script>
